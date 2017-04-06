3d37e5c0921d1131d80e35181070150103524a67
Luis Emilio Juarez Juarez <Luis Emilio Juarez Juarez>
2017-04-06 11:13:11 -0600

    upload example of how remote process is created on server/client aplicattion

---
+++ ServerRPC/Cliente/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using Libreria;
+
+namespace Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClientChannel chanel = new TcpClientChannel();
+            ChannelServices.RegisterChannel(chanel, false);
+            RemotingConfiguration.RegisterWellKnownClientType(
+                typeof(Class1),"tcp://localhost:1235/calculadora");
+            try
+            {
+                while (true)
+                {
+                    Class1 op = new Class1();
+                    Console.WriteLine(op.Suma(4, 3));
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
---
+++ ServerRPC/ServerRPC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels.Tcp;
+
+
+namespace ServerRPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemotingConfiguration.RegisterWellKnownServiceType(
+            typeof(Libreria.Class1),"calculadora", 
+            WellKnownObjectMode.SingleCall);
+            TcpChannel chnl = new TcpChannel(1235);
+            Console.WriteLine("estamos escuchando");
+            Console.ReadKey();
+        }
+    }
+}
